b63e746ebd32e8df77cd6e16c56bb52461ef3de3
prasad1057 <khandagaleprasad188@gmail.com>
2026-02-08 13:03:13 +0530

    looping concept is done

---
+++ Core_Python/Demo/Branching/if.py
@@ -0,0 +1,28 @@
+# Syntax
+# if(condition):
+#     block of Code
+
+# if
+'''
+num = int(input("Enter a number : "))
+if(num%2 == 0):
+    print(f'{num} is an Even Number')       # no output if num is odd
+'''
+    
+# if_else
+'''
+numm = int(input("Enter a number : "))
+if(numm%2 == 0):
+    print(f'{numm} is an Even Number')       
+else:
+    print(f"{numm} is odd number")
+'''
+    
+# WAP to check number is positive or negative
+'''
+n1 = int(input(" Enter a number : "))
+if (n1 > 0):
+    print(f"{n1} is a +ve number")
+else :
+    print(f"{n1} is a -ve number")
+'''
---
+++ Core_Python/Demo/Branching/if_else_ladder.py
@@ -0,0 +1,9 @@
+# if_else Ladder
+
+num1 = int(input("Enter the Number : "))
+if (num1 < 0):
+    print(f"Number is negative")
+elif (num1 == 0):
+    print("Number is neutral")
+else :
+    print("Number is Positive")
