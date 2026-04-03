79f8cc2b7efc2b040356473ef2ca4c1136b26daa
prasad1057 <khandagaleprasad188@gmail.com>
2026-04-03 21:04:30 +0530

    python data structure is done

---
+++ Core_Python/Assignements/Assignment09-11-3-26/10_rev_no.py
@@ -0,0 +1,15 @@
+# 10. Write a program to reverse a number using recursion.
+
+def reverse_num(n,rev=0):
+    if n == 0:
+        return rev
+    else:
+        digit = n % 10
+        rev = rev * 10 + digit
+        return reverse_num(n // 10 , rev)
+
+num = int(input("Enter a number: "))
+
+res = reverse_num(num)
+
+print("Reversed number: ",res)
