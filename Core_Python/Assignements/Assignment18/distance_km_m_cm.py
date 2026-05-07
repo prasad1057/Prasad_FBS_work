a61b2f511124030c39b5e555163e5a5c8a0970b8
prasad1057 <khandagaleprasad188@gmail.com>
2026-05-07 22:22:55 +0530

    class assignment done

--- Core_Python/Assignements/Assignment18/distance_km_m_cm.py
+++ Core_Python/Assignements/Assignment18/distance_km_m_cm.py
@@ -21,8 +21,16 @@ class Distance:
         return Distance(k,m,c)
     
     
+    def __sub__(self, other):
+        k = self.kilometer - other.kilometer
+        m = self.meter - other.meter
+        c = self.centimeter - other.centimeter
+        
+        return Distance(k, m, c)
+    
+    
     def __str__(self):
-        return f"{self.kilometer} + {self.meter} + {self.centimeter}"
+        return f"{self.kilometer} km {self.meter} m {self.centimeter} cm"
     
     def __del__(self):
         print("Destructor method of Distance")
@@ -32,4 +40,4 @@ d1 = Distance(10,30,100)
 d2 = Distance(20,50,150)
 
 print("Addition:", d1 + d2)
-        
+print("Subtraction:", d1 - d2)
--- Core_Python/Assignements/Assignment19/Comprehension/03_count_no_user_input.py
+++ Core_Python/Assignements/Assignment19/Comprehension/03_count_no_user_input.py
@@ -1,3 +1,2 @@
 # 3. Count the number of spaces in a string (take input from user)
 
-num1 = [ele for ]
