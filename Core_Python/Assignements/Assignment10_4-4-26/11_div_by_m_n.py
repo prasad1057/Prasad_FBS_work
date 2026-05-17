d5b8487fcc3ad575cfc9b548ca376f20291382c4
prasad1057 <khandagaleprasad188@gmail.com>
2026-05-18 00:02:49 +0530

    adding python assignmnet question

--- Core_Python/Assignements/Assignment10_4-4-26/11_div_by_m_n.py
+++ Core_Python/Assignements/Assignment10_4-4-26/11_div_by_m_n.py
@@ -1,18 +1,30 @@
-# 11. Write a program to print all numbers which are divisible by m and n in the list.
+# Write a program to print all numbers which are divisible by m and n in the list.
 
 def devByMn(list1):
-    
-    m = int(input('enter the value of m: '))
-    n = int(input('enter the value of n: '))
-    
-    print("Numbers divisible by", m, "and", n, "are:")
-    
+
+    m = int(input('Enter the value of m: '))
+    n = int(input('Enter the value of n: '))
+
+    divisible_list = []
+
     for i in list1:
         if i % m == 0 and i % n == 0:
-            print(i) 
+            divisible_list.append(i)
+
+    return divisible_list
 
 
+list1 = []
 
-list1 = [1,2,3,5,6,12,18,24,30,21,45,60]
+size = int(input("Enter number of elements: "))
 
-devByMn(list1)
+for i in range(size):
+    ele = int(input("Enter number that you add in list: "))
+    list1.append(ele)
+
+result = devByMn(list1)
+
+print("Original List:", list1)
+print("Numbers divisible by both m and n:", result)
+
+# list1 = [1,2,3,5,6,12,18,24,30,21,45,60]
